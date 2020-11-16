50022f8adad6798f5e58a3c36eaa9fabc18cb891
Tom Reed <t.reed1104@gmail.com>
2020-11-16 12:54:39 +0000

    Rewrote the App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,16 @@
-import React from 'react';
- 
-const App = ({ title }) =>
-  <div>{title}</div>;
- 
+import React, {Component} from 'react';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { title: this.props.title };
+    }
+
+    render() {
+        return (
+            <div> { this.state.title } </div>
+        );
+    }
+}
+
 export default App;
