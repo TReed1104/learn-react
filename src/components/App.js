d390477ad93682a9cf5b0780fdc8972e0a7e2608
Tom Reed <t.reed1104@gmail.com>
2020-11-16 13:52:56 +0000

    App Template change - turns out it doesnt need a div wrapper only a <>

--- src/components/App.js
+++ src/components/App.js
@@ -8,9 +8,9 @@ class App extends Component {
 
     render() {
         return (
-            <div>
+            <>
                 <h1>{ this.state.title }</h1>
-            </div>
+            </>
         );
     }
 }
