3f13285b2c767b03cd1cf18bcd011751e6ce5faf
Tom Reed <t.reed1104@gmail.com>
2020-11-17 18:08:22 +0000

    Import examples for JS/TS react components

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 
+// Example imports
+//import Template from './Template.js';
+//import Template from "./Template.tsx";
+
+// Example imports of components with the same name but different compile types
+//import { default as JsTemplate } from './Template.js';
+//import { default as TsTemplate } from "./Template.tsx";
+
 class App extends Component {
     static propTypes = {
         title: PropTypes.string
