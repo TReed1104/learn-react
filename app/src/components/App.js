6c975773809f9c2a69a613a1811027b55859da26
Tom Reed <t.reed1104@gmail.com>
2020-11-24 12:35:45 +0000

    Implemented the procedural generation of the links and react-router switch

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -24,6 +24,25 @@ class App extends Component {
     }
 
     render() {
+        // Procedurally generate our links and setup the Router
+        let linkElements = [];
+        let routeElements = [];
+        const routes = [
+            { id: "home", to: "/", text: "Home", component: Home, exact: true },
+        ];
+        for (let route of routes) {
+            // Create our links
+            linkElements.push(<Link key={route.id} to={route.to}>{route.text}</Link>);
+            // Generate the Route elements for the <Switch>
+            if (route.exact) {
+                routeElements.push(<Route key={route.id} path={route?.to} component={route.component} exact/>);
+            }
+            else {
+                routeElements.push(<Route key={route.id} path={route.to} component={route.component}/>);
+            }
+        }
+
+        // Return the template to render
         return (
             <>
                 <h1>{this.props.title}</h1>
