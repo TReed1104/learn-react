bde5ab1f5c263dba5e6cded835543666fc54da9a
Tom Reed <t.reed1104@gmail.com>
2020-11-24 12:34:39 +0000

    React router imports

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types'
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom"
 
 // Example imports
 //import Template from './Template.js';
