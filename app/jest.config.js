b2e1da66eb29b1d8165e2974874599e009219013
Tom Reed <t.reed1104@gmail.com>
2020-11-25 10:31:44 +0000

    Added jest config file

---
+++ app/jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    roots: [
+        '<rootDir>'
+    ],
+    transform: {
+        "^.+\\.js$": "babel-jest",
+        '^.+\\.tsx?$': 'ts-jest',
+    },
+    moduleNameMapper: {
+        "^.+\\.(css|less|scss|sass)$": "identity-obj-proxy"
+    },
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+    moduleFileExtensions: [
+        'ts',
+        'tsx',
+        'js',
+        'jsx',
+        'json',
+        'node'
+    ]
+}
